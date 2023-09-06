6598dcda012c8705b2e5b38292044ef125194813
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-09-06 10:27:46 -0600

    Write tests for iteration 4

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -62,4 +62,42 @@ RSpec.describe GradeBook do
     end
   end
 
+  describe '#all_grades' do
+    it 'can list all grades in a hash sorted by course' do
+      @gradebook.add_course(@course1)
+      @gradebook.add_course(@course2)
+      @student1.log_score(50)
+      @student1.log_score(45)
+      @student2.log_score(90)
+      @student2.log_score(90)
+      @student3.log_score(80)
+      @student3.log_score(90)
+      @student4.log_score(91)
+      @student4.log_score(1)
+      @student4.log_score(2)
+      expect(@gradebook.all_grades).to eq({
+        @course1 => [@student1.grade, @student2.grade],
+        @course2 => [@student3.grade, @student4.grade, @student1.grade]
+      })
+    end
+  end
+
+  describe '#students_in_range' do
+    it 'can list all students in between two numbers' do
+      @gradebook.add_course(@course1)
+      @gradebook.add_course(@course2)
+      @student1.log_score(50)
+      @student1.log_score(45)
+      @student2.log_score(90)
+      @student2.log_score(90)
+      @student3.log_score(80)
+      @student3.log_score(90)
+      @student4.log_score(91)
+      @student4.log_score(1)
+      @student4.log_score(2)
+      expect(@gradebook.students_in range(10-50)).to eq([@student1, @student4])
+      expect(@gradebook.students_below(60-89)).to eq([@student3])
+      expect(@gradebook.students_below(60-91)).to eq([@student2, @student3])
+    end
+  end
 end
