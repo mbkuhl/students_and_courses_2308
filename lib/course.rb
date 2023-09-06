915c7616fdaa33f07ccbed7da889689fbfe12e80
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-09-06 09:38:47 -0600

    Pass all tests for iteration 2

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,23 @@
 class Course
+  attr_reader :name, :capacity, :students
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @students = []
+    @full = false
+  end
+
+  def full?
+    if @students.count < @capacity
+      @full = false
+    else
+      @full = true
+    end
+  end
   
+  def enroll(student)
+    if @full == false
+      @students << student
+    end
+  end
 end
